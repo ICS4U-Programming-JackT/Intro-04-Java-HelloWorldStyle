a7c666549ac265c94473eb4fc3c33c327466f42b
Jack Turcotte <jack.turcotte@stu.ocsb.ca>
2025-09-10 00:14:20 +0000

    finished

---
+++ HelloWorldStyle.java
@@ -0,0 +1,12 @@
+/**
+* Print hello world in java
+* @author  Jack Turcotte
+* @version 1.0
+* @since   2025-09-05
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World (with style!)");
+    }
+}
