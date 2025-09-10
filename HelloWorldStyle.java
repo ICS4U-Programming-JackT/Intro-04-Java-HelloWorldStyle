4b40e47a1cedb7c1bc3ab00f95220ab7bc02f81b
Jack Turcotte <jack.turcotte@stu.ocsb.ca>
2025-09-10 02:10:01 +0000

    Added style fix

--- HelloWorldStyle.java
+++ HelloWorldStyle.java
@@ -1,12 +1,32 @@
 /**
-* Print hello world in java
+* Print hello world in java.
 * @author  Jack Turcotte
 * @version 1.0
 * @since   2025-09-05
 */
 
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World (with style!)");
+// Defines the class
+public final class HelloWorldStyle {
+
+    /**
+     * This is a private constructor used to
+     * satisfy the style checker.
+     * @exception IllegalStateException Utility Class
+     * @see IllegalStateException
+     */
+    private HelloWorldStyle() {
+        throw new IllegalStateException("Utility Class");
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args Unused.
+     */
+
+    public static void main(final String[] args) {
+        // Prints two text lines
+        System.out.println("Hello, world!");
+        System.out.println("This is hello world with style!");
     }
 }
